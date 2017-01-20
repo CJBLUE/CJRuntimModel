a0af665c07967cab785db658dcb483d457edf393
zxjk <zxjk@zxjks-imac.local>
2017-01-20 10:20:00 +0800

    Initial Commit

---
+++ CJRuntimModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJRuntimModel
+//
+//  Created by zxjk on 17/1/20.
+//  Copyright © 2017年 zxjk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CJRuntimModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CJRuntimModel
+//
+//  Created by zxjk on 17/1/20.
+//  Copyright © 2017年 zxjk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
